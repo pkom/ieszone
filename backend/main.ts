fbbf1587e3be45c034e2da58ffada0de82db0a79
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-12-22 20:14:19 +0100

    add global validations pipe to backend

--- backend/main.ts
+++ backend/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from './config';
@@ -5,6 +6,16 @@ import { config } from './config';
 async function bootstrap() {
   const { appName, port, version } = config;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.setGlobalPrefix(version);
   await app.listen(port, () => {
     console.info(

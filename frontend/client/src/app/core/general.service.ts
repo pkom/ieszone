35fa8bf02e39e236a0eb29b4aaf5fd1a89d6f913
Francisco Mora <francisco.mora.sanchez@gmail.com>
2021-01-25 14:35:10 +0100

    add work

--- frontend/client/src/app/core/general.service.ts
+++ frontend/client/src/app/core/general.service.ts
@@ -3,7 +3,6 @@ import { environment } from '@env';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpService } from '@core/http.service';
 import { Config, Course } from './config.model';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,9 +11,6 @@ export class GeneralService {
   static END_POINT_CONFIG = environment.REST_USER + '/configuration';
   static END_POINT_COURSES = environment.REST_USER + '/courses';
   static END_POINT_STATUS = environment.REST_USER;
-
-  private config: Config;
-  private courses: Course[];
 
   appStatus$ = this.httpService.get(GeneralService.END_POINT_STATUS);
   darkTheme$ = new BehaviorSubject<boolean>(false);
@@ -31,20 +27,10 @@ export class GeneralService {
   }
 
   getConfig(): Observable<Config> {
-    return this.httpService.get(GeneralService.END_POINT_CONFIG).pipe(
-      map((config) => {
-        this.config = config;
-        return this.config;
-      }),
-    );
+    return this.httpService.get(GeneralService.END_POINT_CONFIG);
   }
 
   getCourses(): Observable<Course[]> {
-    return this.httpService.get(GeneralService.END_POINT_COURSES).pipe(
-      map((courses) => {
-        this.courses = courses;
-        return this.courses;
-      }),
-    );
+    return this.httpService.get(GeneralService.END_POINT_COURSES);
   }
 }

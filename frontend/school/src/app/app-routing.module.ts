9d5bf23875918d189dd7a67fe19b5f0b54e49cef
Francisco Mora <francisco.mora.sanchez@gmail.com>
2021-02-05 13:27:40 +0100

    add work

--- frontend/school/src/app/app-routing.module.ts
+++ frontend/school/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: 'dashboard',

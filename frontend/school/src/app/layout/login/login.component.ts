a78c234f7f7afa9c585c171fb4ea135d9732a1be
Francisco Mora <francisco.mora.sanchez@gmail.com>
2021-03-23 23:54:56 +0100

    keep working

--- frontend/school/src/app/layout/login/login.component.ts
+++ frontend/school/src/app/layout/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { LoginUser } from '../../setting/+state/config.action';
-import { map, pluck } from 'rxjs/operators';
+import { map, pluck, tap } from 'rxjs/operators';
 import * as fromConfig from '../../setting/+state/config.selector';
 
 @Component({

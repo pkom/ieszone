5b0cee61cb910a0275c1ef857b5101a011c8e646
Francisco Mora <francisco.mora.sanchez@gmail.com>
2021-05-14 14:18:19 +0200

    Some cleanup

--- frontend/school/src/app/layout/login/login.component.ts
+++ frontend/school/src/app/layout/login/login.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { LoginUser } from '../../setting/+state/config.action';
-import { map, pluck, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import * as fromConfig from '../../setting/+state/config.selector';
-import { Course } from '../../core/config.model';
 
 @Component({
   selector: 'z-login',

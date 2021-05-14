f8fc09fbbf21567f0b57898c6409dceee725761a
Francisco Mora <francisco.mora.sanchez@gmail.com>
2021-05-14 14:33:55 +0200

    Avoid change detection error on Login component

--- frontend/school/src/app/layout/login/login.component.ts
+++ frontend/school/src/app/layout/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { LoginUser } from '../../setting/+state/config.action';
@@ -9,6 +9,7 @@ import * as fromConfig from '../../setting/+state/config.selector';
   selector: 'z-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
   config$ = this.store.select(fromConfig.getConfig);
